2eb67299e2886fcce03b188ac53255620e45570e
Joanne Stephany <joannestephany@hotmail.com>
2024-07-29 16:55:51 -0300

    add: init delete classroom to firebase functions

---
+++ functions/src/controller/classroom/deleteClassroom.ts
@@ -0,0 +1,32 @@
+import { Firestore } from "firebase-admin/firestore";
+
+//TODO:: Criar delete no functions, e lembrar de deletar a turma e também os alunos da turma, ou seja, todos os studenst com a classroom_fk que esta sendo apagado
+
+const deleteClassroom = async (firestore: Firestore, id: string) => {
+    try {
+        const refClassroom = await firestore.collection("classroom").doc(id).get();
+        if (!refClassroom.exists) {
+        throw new Error("Turma não encontrada");
+        }
+        await firestore.collection("classroom").doc(id).delete();
+        return "Turma deletada com sucesso";
+    } catch (err) {
+        console.error("Erro ao deletar turma:", err);
+        throw err;
+    }
+    }
+
+export { deleteClassroom };
+
+// export const deleteItem = async (id, history) => {
+//     const itemRef = doc(collection(firestore, "classroom"), id);
+
+//     try {
+//         await deleteDoc(itemRef);
+//         history.push("/turmas")
+//         console.log("Item deleted successfully");
+//     } catch (err) {
+//         console.error("Error deleting item:", err);
+//         throw err;
+//     }
+// }
